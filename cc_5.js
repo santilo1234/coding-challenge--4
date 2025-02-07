87877ed5a43ecb9c221eae5244889d605bcb995a
santiago lopez <slopezarias@usf.edu>
2025-02-07 12:22:29 -0500

    task 1

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+//Task 1
+ const customer= {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+console.log (`name: ${customer.name}`);
+console.log (`age: ${customer.age}`);
+console.log (`email: ${customer.email}`);
